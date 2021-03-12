10846c0f99d76e2e3384d0db6e5426bbb49b9593
Yerin Shin <55073486+rinrin528@users.noreply.github.com>
2021-03-13 00:58:29 +0900

    Update array_test.cpp

--- 0x03/array_test.cpp
+++ 0x03/array_test.cpp
@@ -1,12 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void insert(int idx, int num, int arr[], int& len){
-
+void insert(int idx, int num, int arr[], int& len) {
+	if (idx < len) {
+		for(int i = len; i > idx; i--) arr[i] = arr[i - 1];
+	}
+	arr[idx] = num;
+	len++;
 }
 
-void erase(int idx, int arr[], int& len){
-  
+void erase(int idx, int arr[], int& len) {
+	for (int i = idx; i < len-2; i++) {
+		arr[i] = arr[i + 1];
+	}
+	len--;
 }
 
 void printArr(int arr[], int& len){
